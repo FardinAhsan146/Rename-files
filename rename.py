af7fa822fa0445039891b1565a515f0eb6a99d63
FardinAhsan146 <69723973+FardinAhsan146@users.noreply.github.com>
2021-03-08 05:22:56 -0800

    Update rename.py

--- rename.py
+++ rename.py
@@ -5,6 +5,7 @@ import os
 dir_name = #enter path  here as a raw string
 
 #Insert newfile names here as strings, if your file name has an extension just append the extension at the end of every item in the list
+#for example I want to name a folder of videos from 1 to 10, my list would be [str(i) + ".mp4" for i in range(1,11)]
 newnames_list = []
 
 for index,file in enumerate(os.listdir(dir_name)): 
